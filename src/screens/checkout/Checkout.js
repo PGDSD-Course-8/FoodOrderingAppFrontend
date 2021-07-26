97d91df7098eac97db4e65bb8c8924c1870be7bf
Archita Jain <sanatt.abrol.in@gmail.com>
2021-07-26 16:15:25 +0530

    Added Navigation to checkout

---
+++ src/screens/checkout/Checkout.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Checkout extends Component {
+    constructor() {
+        super();
+    }
+
+
+    render() {
+        return(
+            <div className="checkout">
+                Checkout Page 
+            </div>
+        );
+    }
+}
+
+export default Checkout;
