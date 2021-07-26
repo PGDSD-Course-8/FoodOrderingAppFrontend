f2e069bf99447cb7a274845e4f1c18474dbfc881
Archita Jain <jainarchita12@gmail.com>
2021-07-26 23:07:36 +0530

    Added explanations for placing order

--- src/common/orderitems/OrderItems.js
+++ src/common/orderitems/OrderItems.js
@@ -1,13 +1,11 @@
-import React, {Fragment} from 'react'
-//Stylesheet imports
+import React, {Fragment} from 'react';
+import Grid from "@material-ui/core/Grid";
 import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 import './OrderItems.css'
-//Material UI imports
 import Typography from "@material-ui/core/Typography";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
 import FormControl from "@material-ui/core/FormControl";
-import Grid from "@material-ui/core/Grid";
 
 export default function OrderItems(props) {
     return (
@@ -17,15 +15,18 @@ export default function OrderItems(props) {
                     <Grid item xs={1}>
                         <i className="fa fa-stop-circle-o icon-type" aria-hidden="true"
                            style={item.type === 'VEG' ? {color: "green"} : {color: "red"}}/>
+                           {/* Green is displayed for the veg items and red for non-veg items */}
                     </Grid>
                     <Grid item xs={6}>
                         <Typography color='textSecondary' style={{textTransform: "capitalize"}}>
                             {item.name}
+                            {/* Name of each item is displayed */}
                         </Typography>
                     </Grid>
                     <Grid item xs={2}>
                         <Typography color='textSecondary'>
                             {item.quantity}
+                            {/* Quantity of each item is displayed */}
                         </Typography>
                     </Grid>
                     <Grid item xs={3}>
@@ -37,12 +38,14 @@ export default function OrderItems(props) {
                 </Grid>
             ))
             }
-            {props.divider ? <Divider style={{marginTop: 15, marginBottom: 15}}/> : null}
+            {props.divider ?
+             <Divider style={{marginTop: 15, marginBottom: 15}}/>
+              : null
+            }
             <Grid container>
+                {/* Displays the bill amount for the ordered items */}
                 <Grid item xs={9}>
-                    <Typography color='textPrimary'>
-                        Net Amount
-                    </Typography>
+                    <Typography color='textPrimary'>  Net Amount </Typography> 
                 </Grid>
                 <Grid item xs={3}>
                     <div className='payable-bill-amount'>
@@ -58,7 +61,10 @@ export default function OrderItems(props) {
             <Grid container>
                 <Grid item xs={12}>
                     <FormControl className='place-order-button'>
-                        <Button variant="contained" color="primary" onClick={props.placeOrder}>PLACE ORDER</Button>
+                        <Button variant="contained" color="primary" onClick={props.placeOrder}>
+                            PLACE ORDER
+                            {/* Used for placing order */}
+                        </Button>
                     </FormControl>
                 </Grid>
             </Grid>
